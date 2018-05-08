32a6426de21eea2ae6a31077973fdbed2dd83187
tim <1510899867@qq.com>
2018-05-08 15:09:29 +0800

    EFCore

---
+++ EFCoreOverview/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreOverview.Models
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        public string Name { get; set; }
+        public Sex Sex { get; set; }
+        public Teacher Teacher { get; set; }
+        public string TeacherId { get; set; }
+    }
+
+    public enum Sex
+    {
+        Girl,
+        Boy
+    }
+}
---
+++ EFCoreOverview/Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreOverview.Models
+{
+    public class Teacher
+    {
+        public Teacher()
+        {
+            Students = new List<Student>();
+        }
+        public string TeacherId { get; set; }
+        public string Name { get; set; }
+        public Sex Sex { get; set; }
+        public ICollection<Student> Students { get; protected set; }
+    }
+}
